826fc3fb0329e3e9327e459da0efd4647b149ec7
appyens <an.anurag@msn.com>
2020-02-05 01:05:18 +0530

    notes added

---
+++ configs.py
@@ -0,0 +1,24 @@
+"""A setup file reader for the aws-console module"""
+
+import os
+from configparser import ConfigParser
+
+
+class ConfigReader:
+    """
+    A class to implement custom cfg file reader
+    """
+    def __init__(self):
+        self.cfg_file = os.path.join(os.path.dirname(__file__), 'setup.cfg')
+        self._config = ConfigParser()
+        self._config.read(self.cfg_file)
+
+    def read(self):
+        """
+        A read method to read key and values
+        :return:
+        """
+        return self._config
+
+
+conf = ConfigReader()
